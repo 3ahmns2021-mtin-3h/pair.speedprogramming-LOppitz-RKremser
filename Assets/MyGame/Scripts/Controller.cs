749f895ddbca4140a0a619d8d2121f29e44eb2a3
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2021-05-02 17:11:36 +0200

    update controller script

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+
+    public GameObject blueFlowers;
+
+    private int spacePressed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,26 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnFlowers();
+            spacePressed++;
+        }
+
+        //if (spacePressed == 10)
+        {
+        //    spawnFlowers();
+        }
+
+
+        Debug.Log("Spawn " + spacePressed + " Flowers");
+
+        void spawnFlowers()
+        {
+            var clones = Instantiate(blueFlowers);
+            clones.GetComponent<Transform>().localPosition = new Vector3(Random.Range(-400, 400), 0, 0);
+            clones.GetComponent<Transform>().localRotation = Quaternion.identity;
+            clones.GetComponent<Transform>().localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
     }
 }
